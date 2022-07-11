f19d6516d231d1022bc110a6199f349f49727062
dvaupel <85832165+dvaupel@users.noreply.github.com>
2022-07-11 22:24:09 +0200

    Let img_load detect number of channels

--- src/main.c
+++ src/main.c
@@ -15,10 +15,10 @@ int main(int argc, char* argv[]) {
     }
 
     int width_orig, height_orig, nrChannels_orig;
-    unsigned char* original = stbi_load(argv[1], &width_orig, &height_orig, &nrChannels_orig, 3);
+    unsigned char* original = stbi_load(argv[1], &width_orig, &height_orig, &nrChannels_orig, 0);
     
     int width_deriv, height_deriv, nrChannels_deriv;
-    unsigned char* derivative = stbi_load(argv[2], &width_deriv, &height_deriv, &nrChannels_deriv, 3);
+    unsigned char* derivative = stbi_load(argv[2], &width_deriv, &height_deriv, &nrChannels_deriv, 0);
 
     if (width_orig != width_deriv || height_orig != height_deriv || nrChannels_orig != nrChannels_deriv) {
         fatal("image dimensions/channels must match");
